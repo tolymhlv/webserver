44639d9a64aae0408d911a9786adb55eb967f91c
tolymhlv <tolymhlv@gmail.com>
2017-12-02 17:15:39 +0300

    auth test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,24 +1,38 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
-import java.util.logging.Logger;
+import servlets.SessionServlet;
+import servlets.UsersServlet;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
+        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+        context.addServlet(new ServletHolder(new SessionServlet(accountService)), "/api/v1/sessions");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
 
         Server server = new Server(8080);
-        server.setHandler(context);
+        server.setHandler(handlers);
 
         server.start();
-        Logger.getGlobal().info("Server started");
         server.join();
     }
 }
